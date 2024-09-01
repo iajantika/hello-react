60d42272598869eadbed7f4936fa5c2afb86335a
Ajantika Bandyopadhyay <ajantikabandyopadhyay@Ajantikas-MacBook-Air.local>
2024-09-01 21:12:38 +0530

    episode-01

---
+++ app.js
@@ -0,0 +1,25 @@
+// const parent1 = React.createElement("div", { id: "parent" },
+//     React.createElement("div", { id: "child" }, [
+//         React.createElement("h1", {}, "I am H1 tag"),
+//         React.createElement("h2", {}, "I am H2 tag")
+//     ]
+//     )
+// );
+
+const parent = React.createElement("div", {id: "parent"}, [
+    React.createElement("div", { id: "child1" }, [
+        React.createElement("h1", {}, "I am H1 tag"),
+        React.createElement("h2", {}, "I am H2 tag")]
+    ),
+        React.createElement("div", {}, [
+            React.createElement("h1", {}, "I am ch2 H1 tag"),
+            React.createElement("h2", {}, "I am ch2 H2 tag")
+        ]),
+    ]);
+
+
+const heading = React.createElement("h1", { id: "heading" }, "Hello World from React!");
+console.log(heading);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(parent);
