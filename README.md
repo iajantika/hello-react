ca229f7af21354842e1547fae6032e5a94f5736d
Ajantika Bandyopadhyay <ajantikabandyopadhyay@Ajantikas-MacBook-Air.local>
2024-10-13 00:15:22 +0530

    Episode 2

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # Namastey React
+
+#Parcel
+- Dev Build
+- locl Server
+- HMR = Hot Module Replacement
+- File Watching Algorithm - written in C++
+- Caching - Faster Builds
+- Image Optimization
+- Minification of our file
+- Bundling
+- Compress
+- Consistent Hashing
+- Code Splitting
+- Differencial Bundling
+- Diagonostics
+- Error Handling
+- HTTPs
+- Tree Shaking - remove unused code for us
--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
 // const parent1 = React.createElement("div", { id: "parent" },
 //     React.createElement("div", { id: "child" }, [
 //         React.createElement("h1", {}, "I am H1 tag"),
@@ -6,13 +8,13 @@
 //     )
 // );
 
-const parent = React.createElement("div", {id: "parent"}, [
-    React.createElement("div", { id: "child1" }, [
-        React.createElement("h1", {}, "I am H1 tag"),
+const parent = React.createElement("div", {}, [
+    React.createElement("div", {id: "parent"}, [
+        React.createElement("h1", {}, "I am H1 tag fhgf"),
         React.createElement("h2", {}, "I am H2 tag")]
     ),
-        React.createElement("div", {}, [
-            React.createElement("h1", {}, "I am ch2 H1 tag"),
+        React.createElement("div", {id: "child1"}, [
+            React.createElement("h1", {}, "I am ch2 H1 tag bhjhj"),
             React.createElement("h2", {}, "I am ch2 H2 tag")
         ]),
     ]);
